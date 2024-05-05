7230e6d81a922c4556f8011c569c44bdfe1afb75
rafacabrera7 <rafael.cabrera@urosario.edu.co>
2024-05-04 20:02:43 -0500

    Parcial 3

---
+++ Parcial3/clientsocket_ippiblica.py
@@ -0,0 +1,30 @@
+import socket
+
+# Configuración del servidor y puerto
+#host = 'localhost'
+#port = 5050
+
+host = '0.tcp.ngrok.io'
+port = 16039
+
+
+# Crear el socket del cliente
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Conectarse al servidor
+client_socket.connect((host, port))
+
+while True:
+    # Recibir datos del servidor
+    data = client_socket.recv(1024)
+    if not data:
+        # Si no hay datos, la conexión se ha cerrado
+        print("Conexión cerrada por el servidor.")
+        break
+    # Decodificar los datos y mostrarlos en la consola
+  #  print("Datos recibidos: ", data.decode('utf-8'))
+    print("Datos recibidos: ", data)
+
+# Cerrar el socket del cliente
+client_socket.close()
+
---
+++ Parcial3/sparkstreamingstarterkit.py
@@ -0,0 +1,91 @@
+
+import sys, string
+import os
+import socket
+import time
+import operator
+import boto3
+import json
+# from pyspark.sql import SparkSession
+# from pyspark.streaming import StreamingContext
+
+from pyspark.sql import Row, SparkSession
+from pyspark.sql.streaming import DataStreamWriter, DataStreamReader
+from pyspark.sql.functions import explode ,split ,window
+from pyspark.sql.types import IntegerType, DateType, StringType, StructType
+from pyspark.sql.functions import sum,avg,max, col, count
+
+print()
+print("ESTE SÍ ES")
+print()
+if __name__ == "__main__":
+    
+    spark = SparkSession\
+        .builder\
+        .appName("ParcialStreaming")\
+        .getOrCreate() \
+    
+    spark.sparkContext.setLogLevel("ERROR")
+    # TODO: Define a windowDuration and slideDuration
+    # Se definen dos variables windowDuration y slideDuration, 
+    # que se utilizan más adelante en la consulta de Spark Streaming. 
+    # Estas variables determinan la duración de la ventana y el deslizamiento
+    # para el procesamiento de datos en tiempo real
+    
+    windowDuration = '50 seconds'
+    slideDuration = '25 seconds'
+        
+    # Set up the `logsDF` readStream to take in data from a socket stream, AND include the timestamp
+    # Transformación de  los datos leídos. Se divide cada línea de registro en palabras utilizando split()
+    # y se crea una nueva columna "logs" con el resultado. Luego, se selecciona la columna de marca de tiempo 
+    # (logsDF.timestamp) y se aplica la función explode() para convertir las palabras divididas en filas
+    # separadas con la misma marca de tiempo.
+    
+    data = spark.readStream.format("socket").option("host", '0.tcp.ngrok.io')\
+            .option("multiLine",True)\
+            .option("port", 12990).load()
+
+    data = data.withColumn("valores", split(data.value,','))
+    """,Date Created,Number of Likes,Source of Tweet,Tweet,Sentiment"""
+
+    dataClean = data.withColumn('id', data['valores'].getItem(0)) \
+       .withColumn('date', data['valores'].getItem(1)) \
+       .withColumn('likes', data['valores'].getItem(2)) \
+        .withColumn('source', data['valores'].getItem(3)) \
+        .withColumn('sentiment', data['valores'].getItem(5)) \
+        .drop(data['valores'])\
+        .drop(data['value'])\
+        #.withColumn('Tweet', data['valores'].getItem(4)) \
+    dataClean = dataClean.filter(col('id').rlike('^[0-9]+$'))
+
+    ## Falta hacerle cast a la column date y luego agrupar por eso.
+
+    result = dataClean.groupBy().agg(count(col('id')).alias('count'), sum('likes'), avg('likes'))
+        
+    # display all unbounded table completely while streaming the hostCountDF on console using output mode "complete"
+
+    query = dataClean.writeStream.outputMode("update")\
+                       .option("truncate", "false")\
+                       .format("console")\
+                       .start()
+
+    ## Practice and observe by uncommenting the below code lines using different outputModel "append" , "update"
+    
+    ### display unbounded table withonly the changes are appended
+    # query = hostCountsDF.writeStream.outputMode("append")\
+    #                    .option("numRows", "100000")\
+    #                    .option("truncate", "false")\
+    #                    .format("console")\
+    #                    .start()
+
+
+    ### display unbounded with only the update 
+    # query = hostCountsDF.writeStream.outputMode("update")\
+    #                    .option("numRows", "100000")\
+    #                    .option("truncate", "false")\
+    #                    .format("console")\
+    #                    .start()
+
+    
+    #run the query 
+    query.awaitTermination()
