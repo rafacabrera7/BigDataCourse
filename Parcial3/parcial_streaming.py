20d56e3581d2e9a35dc2e2f1289aa4b638a9320c
rafacabrera7 <rafael.cabrera@urosario.edu.co>
2024-05-09 19:07:19 -0500

    Respuesta final

---
+++ Parcial3/parcial_streaming.py
@@ -0,0 +1,70 @@
+
+import sys, string
+import os
+import socket
+import time
+import operator
+import boto3
+import json
+
+from pyspark.sql import Row, SparkSession
+from pyspark.sql.streaming import DataStreamWriter, DataStreamReader
+from pyspark.sql.functions import explode ,split ,window, concat_ws
+from pyspark.sql.types import IntegerType, DateType, StringType, StructType, TimestampType
+from pyspark.sql.functions import sum,avg,max, col, count
+import time
+
+
+print()
+print("ESTE SÍ ES")
+print()
+if __name__ == "__main__":
+    
+    spark = SparkSession\
+        .builder\
+        .appName("ParcialStreaming")\
+        .getOrCreate() \
+    
+    spark.sparkContext.setLogLevel("ERROR")
+    
+    windowDuration = '60 seconds'
+    
+    data = spark.readStream.format("socket").option("host", '8.tcp.ngrok.io')\
+            .option("multiLine",True)\
+            .option("port", 11802).load()
+
+    data = data.withColumn("valores", split(data.value,','))
+
+    dataClean = data.withColumn('id', data['valores'].getItem(0)) \
+       .withColumn('date', data['valores'].getItem(1)) \
+       .withColumn('likes', data['valores'].getItem(2)) \
+        .drop(data['valores'])\
+        .drop(data['value'])\
+
+    dataClean = dataClean.filter(col('id').rlike('^[0-9]+$'))
+    dataClean = dataClean.withColumn("id", dataClean["id"].cast(IntegerType())) \
+                     .withColumn("likes", dataClean["likes"].cast(IntegerType())) \
+                     .withColumn("date", dataClean["date"].cast(TimestampType()))
+
+    result = dataClean.groupBy(window(dataClean.date, windowDuration, 0)) \
+                  .agg(count("id").alias("count"), sum("likes").alias("total_likes"), avg("likes").alias("avg_likes"))
+
+    def guardarBatch(df, batch):
+        df = df.withColumn("window", df["window"].cast(StringType())) \
+                     .withColumn("count", df["count"].cast(StringType())) \
+                     .withColumn("total_likes", df["total_likes"].cast(StringType())) \
+                     .withColumn("avg_likes", df["avg_likes"].cast(StringType()))
+        
+        df = df.select(concat_ws(",", *df.columns).alias("output_value"))
+        df.coalesce(1).write.text(f"s3://outputparcialstreaming/output/batch_{batch}")
+
+    query = result.writeStream.queryName("queryParcial") \
+                          .outputMode("complete") \
+                          .format("console") \
+                          .option("truncate", "false") \
+                          .foreachBatch(guardarBatch) \
+                          .start()
+    
+    #run the query 
+    query.awaitTermination(timeout = 100)
+    query.stop()
