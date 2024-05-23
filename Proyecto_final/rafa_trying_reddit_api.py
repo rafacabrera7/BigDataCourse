af4d10e8a29e2c30ade03810724a862147d8693b
rafacabrera7 <rafael.cabrera@urosario.edu.co>
2024-05-23 17:19:26 -0500

    Gaming dataset

--- Proyecto_final/rafa_trying_reddit_api.py
+++ Proyecto_final/rafa_trying_reddit_api.py
@@ -1,11 +1,23 @@
 import tensorflow_datasets as tfds
 import pandas as pd
 
-# Load the 'reddit' dataset
-ds = tfds.load('reddit', split='test')
+# Load the dataset with streaming enabled
+ds = tfds.load('reddit', split='test[:1%]')
 
-# Convert the TensorFlow dataset to a pandas DataFrame
-df = pd.DataFrame(list(tfds.as_dataframe(ds)))
+# Process examples in batches and save periodically to CSV
+batch_size = 1000  # Number of examples to process at a time
+batch_count = 0
+df_list = []
 
-# Save the DataFrame to a CSV file
-df.to_csv('reddit_dataset.csv', index=False)
+for example in tfds.as_numpy(ds):
+    df_list.append(pd.DataFrame([example]))
+    if len(df_list) >= batch_size:
+        df_batch = pd.concat(df_list, ignore_index=True)
+        df_batch.to_csv(f'reddit_dataset_part_{batch_count}.csv', index=False)
+        batch_count += 1
+        df_list = []  # Reset list for the next batch
+
+# Save any remaining examples
+if df_list:
+    df_batch = pd.concat(df_list, ignore_index=True)
+    df_batch.to_csv(f'reddit_dataset_part_{batch_count}.csv', index=False)
